363e28518a9cf52123547d15fd0ad098adb9f487
Josh Skeen <josh@joshskeen.com>
2014-04-24 10:40:52 -0400

    makes NETWORK REQUEST to load a QWUBBLE

--- src/com/example/myapp/MyActivity.java
+++ src/com/example/myapp/MyActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapp;
 
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.widget.Toast;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -14,8 +15,8 @@ import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.entity.shape.IAreaShape;
 import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
@@ -24,61 +25,46 @@ import org.andengine.extension.physics.box2d.util.Vector2Pool;
 import org.andengine.input.sensor.acceleration.AccelerationData;
 import org.andengine.input.sensor.acceleration.IAccelerationListener;
 import org.andengine.input.touch.TouchEvent;
-import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.ITexture;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.bitmap.BitmapTexture;
+import org.andengine.opengl.texture.region.TextureRegion;
+import org.andengine.opengl.texture.region.TextureRegionFactory;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.adt.io.in.IInputStreamOpener;
 import org.andengine.util.debug.Debug;
 
-import static org.andengine.extension.physics.box2d.util.constants.PhysicsConstants.PIXEL_TO_METER_RATIO_DEFAULT;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MyActivity extends SimpleBaseGameActivity implements IAccelerationListener, IOnSceneTouchListener {
-    // ===========================================================
-    // Constants
-    // ===========================================================
-
-    private static final int CAMERA_WIDTH = 720;
-    private static final int CAMERA_HEIGHT = 480;
+    private static final int CAMERA_WIDTH = 480;
+    private static final int CAMERA_HEIGHT = 720;
 
     private static final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
 
-    // ===========================================================
-    // Fields
-    // ===========================================================
-
     private BitmapTextureAtlas mBitmapTextureAtlas;
 
-    private TiledTextureRegion mBoxFaceTextureRegion;
     private TiledTextureRegion mCircleFaceTextureRegion;
-    private TiledTextureRegion mTriangleFaceTextureRegion;
-    private TiledTextureRegion mHexagonFaceTextureRegion;
 
     private Scene mScene;
 
     private PhysicsWorld mPhysicsWorld;
     private int mFaceCount = 0;
 
-    // ===========================================================
-    // Constructors
-    // ===========================================================
-
-    // ===========================================================
-    // Getter & Setter
-    // ===========================================================
-
-    // ===========================================================
-    // Methods for/from SuperClass/Interfaces
-    // ===========================================================
-
     @Override
     public EngineOptions onCreateEngineOptions() {
         Toast.makeText(this, "Touch the screen to add objects.", Toast.LENGTH_LONG).show();
 
         final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
 
-        return new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera);
     }
 
     @Override
@@ -86,12 +72,10 @@ public class MyActivity extends SimpleBaseGameActivity implements IAccelerationL
 
         BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 
-        this.mBitmapTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 64, 128, TextureOptions.BILINEAR);
-        this.mBoxFaceTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "face_box_tiled.png", 0, 0, 2, 1); // 64x32
-        this.mCircleFaceTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "face_circle_tiled.png", 0, 32, 2, 1); // 64x32
-        this.mTriangleFaceTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "face_triangle_tiled.png", 0, 64, 2, 1); // 64x32
-        this.mHexagonFaceTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "face_hexagon_tiled.png", 0, 96, 2, 1); // 64x32
-        this.mBitmapTextureAtlas.load();
+//        this.mBitmapTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 256, 256, TextureOptions.BILINEAR);
+//        this.mCircleFaceTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "headtest.png", 0, 64, 2, 1); // 64x32
+//
+//        this.mBitmapTextureAtlas.load();
     }
 
     @Override
@@ -128,8 +112,8 @@ public class MyActivity extends SimpleBaseGameActivity implements IAccelerationL
 
     @Override
     public boolean onSceneTouchEvent(final Scene pScene, final TouchEvent pSceneTouchEvent) {
-        if(this.mPhysicsWorld != null) {
-            if(pSceneTouchEvent.isActionDown()) {
+        if (this.mPhysicsWorld != null) {
+            if (pSceneTouchEvent.isActionDown()) {
                 this.addFace(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
                 return true;
             }
@@ -174,94 +158,58 @@ public class MyActivity extends SimpleBaseGameActivity implements IAccelerationL
         final AnimatedSprite face;
         final Body body;
 
-        if(this.mFaceCount % 4 == 0) {
-            face = new AnimatedSprite(pX, pY, this.mBoxFaceTextureRegion, this.getVertexBufferObjectManager());
-            body = PhysicsFactory.createBoxBody(this.mPhysicsWorld, face, BodyDef.BodyType.DynamicBody, FIXTURE_DEF);
-        } else if (this.mFaceCount % 4 == 1) {
-            face = new AnimatedSprite(pX, pY, this.mCircleFaceTextureRegion, this.getVertexBufferObjectManager());
-            body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, face, BodyDef.BodyType.DynamicBody, FIXTURE_DEF);
-        } else if (this.mFaceCount % 4 == 2) {
-            face = new AnimatedSprite(pX, pY, this.mTriangleFaceTextureRegion, this.getVertexBufferObjectManager());
-            body = MyActivity.createTriangleBody(this.mPhysicsWorld, face, BodyDef.BodyType.DynamicBody, FIXTURE_DEF);
-        } else {
-            face = new AnimatedSprite(pX, pY, this.mHexagonFaceTextureRegion, this.getVertexBufferObjectManager());
-            body = MyActivity.createHexagonBody(this.mPhysicsWorld, face, BodyDef.BodyType.DynamicBody, FIXTURE_DEF);
-        }
+        new AsyncTask<Void, Void, TextureRegion>(){
+            TextureRegion imageFromWebservice;
 
-        face.animate(200);
+            @Override
+            protected TextureRegion doInBackground(Void... params) {
+                try {
 
-        this.mScene.attachChild(face);
-        this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(face, body, true, true));
+                    ITexture mTexture = new BitmapTexture(getTextureManager(), new IInputStreamOpener() {
+                        @Override
+                        public InputStream open() throws IOException {
+
+                            URL url = new URL("http://img2.wikia.nocookie.net/__cb20130523224933/achille12345/images/5/51/Head.png");
+
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                            connection.setDoInput(true);
+                            connection.connect();
+                            InputStream input = connection.getInputStream();
+                            BufferedInputStream in = new BufferedInputStream(input);
+                            return in;
+                        }
+                    });
+                    mTexture.load();
+                    imageFromWebservice = TextureRegionFactory.extractFromTexture(mTexture);
+
+                } catch (IOException e) {
+                    Debug.e(e);
+                }
+                return imageFromWebservice;
+            }
+
+            @Override
+            protected void onPostExecute(TextureRegion textureRegion) {
+
+                VertexBufferObjectManager vertexBufferObjectManager = getVertexBufferObjectManager();
+//                face = new AnimatedSprite(pX, pY, textureRegion, this.MyActivity.getVertexBufferObjectManager());
+
+                //create face from TextureRegion
+
+                Sprite entity = new Sprite(pX, pY, textureRegion, getVertexBufferObjectManager());
+
+                Body circleBody = PhysicsFactory.createCircleBody(mPhysicsWorld, entity, BodyDef.BodyType.DynamicBody, FIXTURE_DEF);
+
+//                face.animate(200);
+
+                mScene.attachChild(entity);
+                mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(entity, circleBody, true, true));
+
+            }
+        }.execute();
+
+
     }
 
-    /**
-     * Creates a {@link Body} based on a {@link com.badlogic.gdx.physics.box2d.PolygonShape} in the form of a triangle:
-     * <pre>
-     *  /\
-     * /__\
-     * </pre>
-     */
-    private static Body createTriangleBody(final PhysicsWorld pPhysicsWorld, final IAreaShape pAreaShape, final BodyDef.BodyType pBodyType, final FixtureDef pFixtureDef) {
-		/* Remember that the vertices are relative to the center-coordinates of the Shape. */
-        final float halfWidth = pAreaShape.getWidthScaled() * 0.5f / PIXEL_TO_METER_RATIO_DEFAULT;
-        final float halfHeight = pAreaShape.getHeightScaled() * 0.5f / PIXEL_TO_METER_RATIO_DEFAULT;
 
-        final float top = -halfHeight;
-        final float bottom = halfHeight;
-        final float left = -halfHeight;
-        final float centerX = 0;
-        final float right = halfWidth;
-
-        final Vector2[] vertices = {
-                new Vector2(centerX, top),
-                new Vector2(right, bottom),
-                new Vector2(left, bottom)
-        };
-
-        return PhysicsFactory.createPolygonBody(pPhysicsWorld, pAreaShape, vertices, pBodyType, pFixtureDef);
-    }
-
-    /**
-     * Creates a {@link Body} based on a {@link com.badlogic.gdx.physics.box2d.PolygonShape} in the form of a hexagon:
-     * <pre>
-     *  /\
-     * /  \
-     * |  |
-     * |  |
-     * \  /
-     *  \/
-     * </pre>
-     */
-    private static Body createHexagonBody(final PhysicsWorld pPhysicsWorld, final IAreaShape pAreaShape, final BodyDef.BodyType pBodyType, final FixtureDef pFixtureDef) {
-		/* Remember that the vertices are relative to the center-coordinates of the Shape. */
-        final float halfWidth = pAreaShape.getWidthScaled() * 0.5f / PIXEL_TO_METER_RATIO_DEFAULT;
-        final float halfHeight = pAreaShape.getHeightScaled() * 0.5f / PIXEL_TO_METER_RATIO_DEFAULT;
-
-		/* The top and bottom vertex of the hexagon are on the bottom and top of hexagon-sprite. */
-        final float top = -halfHeight;
-        final float bottom = halfHeight;
-
-        final float centerX = 0;
-
-		/* The left and right vertices of the heaxgon are not on the edge of the hexagon-sprite, so we need to inset them a little. */
-        final float left = -halfWidth + 2.5f / PIXEL_TO_METER_RATIO_DEFAULT;
-        final float right = halfWidth - 2.5f / PIXEL_TO_METER_RATIO_DEFAULT;
-        final float higher = top + 8.25f / PIXEL_TO_METER_RATIO_DEFAULT;
-        final float lower = bottom - 8.25f / PIXEL_TO_METER_RATIO_DEFAULT;
-
-        final Vector2[] vertices = {
-                new Vector2(centerX, top),
-                new Vector2(right, higher),
-                new Vector2(right, lower),
-                new Vector2(centerX, bottom),
-                new Vector2(left, lower),
-                new Vector2(left, higher)
-        };
-
-        return PhysicsFactory.createPolygonBody(pPhysicsWorld, pAreaShape, vertices, pBodyType, pFixtureDef);
-    }
-
-    // ===========================================================
-    // Inner and Anonymous Classes
-    // ===========================================================
 }
